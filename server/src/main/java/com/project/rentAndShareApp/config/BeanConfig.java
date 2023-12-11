066bb3aa389aabf2e876c513324d536befc36962
Oborin Nikita <oborinikita@yandex.ru>
2023-12-11 19:53:24 +0700

    ref: добавил многомодульный проект maven

--- server/src/main/java/com/project/rentAndShareApp/config/BeanConfig.java
+++ server/src/main/java/com/project/rentAndShareApp/config/BeanConfig.java
@@ -11,7 +11,7 @@ public class BeanConfig {
 
     @Bean(initMethod = "start", destroyMethod = "stop")
     public Server inMemoryH2DatabaseaServer() throws SQLException {
-        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9191");
     }
 
 }
